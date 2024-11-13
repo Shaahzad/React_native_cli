158b2ccb82acd359feae786ea6a8fa0cf88dd542
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-13 22:51:37 +0500

    DOne

--- App.jsx
+++ App.jsx
@@ -1,12 +1,13 @@
 import { View, Text, StatusBar, Image } from 'react-native'
 import React from 'react'
 import { Colors } from 'react-native/Libraries/NewAppScreen'
+import Login from './src/screens/Login'
 
 const App = () => {
   return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center', backgroundColor: '#00ff99'}}>
+    <View style={{flex:1}}>
     <StatusBar backgroundColor={'#00ff99'}/>
-    <Image source={require('./src/assets/shahzad.jpg')} resizeMode='contain' style={{width:200, height:200}}/>
+    <Login/>
     </View>
   )
 }
---
+++ src/screens/Login.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <View style={{flex:1}}>
+        <View style={{paddingHorizontal:40, paddingVertical:50}}>
+      <Text style={{fontSize:30, color:'#00ff99', fontWeight:'bold'}}>WhatsApp</Text>
+        </View>
+    </View>
+  )
+}
+
+export default Login
