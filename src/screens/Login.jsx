053cbe4a80862419068fd8a2a42d6efebd8cbb22
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-13 23:21:40 +0500

    Done

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,13 +1,23 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, ScrollView } from 'react-native'
 import React from 'react'
 
 const Login = () => {
   return (
-    <View style={{flex:1}}>
-        <View style={{paddingHorizontal:40, paddingVertical:50}}>
+    <ScrollView style={{flex:1}} showsVerticalScrollIndicator={false}>
+    <View style={{paddingHorizontal:40, paddingVertical:30}}>
       <Text style={{fontSize:30, color:'#00ff99', fontWeight:'bold'}}>WhatsApp</Text>
-        </View>
-    </View>
+      </View>
+      <View style={{flex:1, justifyContent:'center', paddingHorizontal: 20, gap:40, marginTop:20}}>
+        <TextInput placeholder='Phone number or email' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
+        <TextInput placeholder='Phone number or email' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
+        <TextInput placeholder='Phone number or email' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
+        <TextInput placeholder='Phone number or email' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
+        <TextInput placeholder='Password' secureTextEntry={true} style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
+        <TouchableOpacity style={{backgroundColor:'#00ff99', padding:12, borderRadius:10}}>
+        <Text>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   )
 }
 
