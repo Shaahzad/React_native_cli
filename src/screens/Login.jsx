6f06851ce1cbb966f989e570440f7f8a34ea0d58
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-15 11:56:56 +0500

    Done

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -10,15 +10,15 @@ const Login = () => {
       <View style={{flex:1, justifyContent:'center', paddingHorizontal: 20, gap:40, marginTop:20}}>
         <TextInput placeholder='Phone number or email' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
         <TextInput placeholder='Phone number or email' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
-        <TextInput placeholder='Phone number or email' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
-        <TextInput placeholder='Phone number or email' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
+        <TextInput placeholder='Phone number or email' keyboardType='number-pad' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
+        <TextInput placeholder='Phone number or email' keyboardType='email-address' style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
         <TextInput placeholder='Password' secureTextEntry={true} style={{borderWidth:1, borderColor:'gray', borderRadius:10, paddingHorizontal:10}}/>
         <TouchableOpacity style={{backgroundColor:'#00ff99', padding:12, borderRadius:10}}>
         <Text>Sign Up</Text>
         </TouchableOpacity>
       </View>
       <TouchableOpacity style={{paddingHorizontal:25, paddingVertical:20}}>
-        <Text style={{fontSize:15, color:'gray'}}>login ?</Text>
+        <Text style={{fontSize:15, color:'gray'}}>Already have an account? login</Text>
       </TouchableOpacity>
     </ScrollView>
   )
