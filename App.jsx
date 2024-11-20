4f4e774b8e29fee31fce3937fbed9dc1a844e217
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-20 12:21:24 +0500

    Done

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import { View, StatusBar } from 'react-native'
 import React from 'react'
 import Login from './src/screens/Login'
+import Header from './src/components/Header'
 
 const App = () => {
   return (
     <View style={{flex:1}}>
     <StatusBar backgroundColor={'#00ff99'}/>
-    <Login/>
+    {/* <Login/> */}
+    <Header/>
     </View>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 const Header = () => {
   return (
-    <View>
-      <Text>
-        
-      </Text>
+    <View style={{flex:1, justifyContent:'center', alignItems:'center', gap:20, padding: 20, backgroundColor:'#00ff99'}}>
+      <Text style={{fontSize:40, fontWeight:'bold', color:'white'}}>Coffee Shop</Text>
+      <View>
+        <Image source={{uri:'https://reactnative.dev/img/header_logo.png'}} width={140} height={130}/>
+      </View>
     </View>
   )
 }
