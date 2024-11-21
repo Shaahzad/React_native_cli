19cfced9fa3b08da3f609037f362ac60576c0703
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-21 12:19:25 +0500

    Done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,10 @@ import React from 'react'
 const Header = () => {
   return (
     <View style={{flex:2, justifyContent:'center', alignItems:'center', gap:20, padding: 20, backgroundColor:'#00ff99'}}>
-      <Text style={{fontSize:40, fontWeight:'bold', color:'white'}}>Coffee Shop</Text>
-      <View>
-        <Image source={{uri:'https://reactnative.dev/img/header_logo.png'}} width={140} height={130}/>
+      <Text style={{fontSize:40, fontWeight:'bold', color:'black'}}>Coffee Shop</Text>
+      <View style={{flex:1, justifyContent:'center', alignItems:'center', gap:20}}>
+        <Image source={{uri:'https://reactnative.dev/img/header_logo.png'}} width={150} height={130}/>
+        <Text style={{fontSize:14, fontWeight:'bold', width:400, textAlign:'center', color:'black'}}>React-Native Best For Mobile Application Development</Text>
       </View>
     </View>
   )
