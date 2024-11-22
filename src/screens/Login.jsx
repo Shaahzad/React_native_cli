4d644de3a42e7158d76ed5e4125a503b10340a22
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-22 15:17:01 +0500

    Done

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 const Login = () => {
   return (
     <ScrollView style={{flex:1}}>
-     <Image source={require('../assets/login.png')} style={{width: 400, height: 400, objectFit: 'cover', alignSelf: 'center'}}/>
-      <View style={{justifyContent:'center', alignItems:'center', gap:20, backgroundColor:'#00ff99', paddingVertical: 40, borderTopLeftRadius: 30, borderTopRightRadius: 30}}>
-        <TextInput placeholder='Email' style={{borderWidth: 1, width: 300, height: 50, alignSelf: 'center', borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}/>
-        <TextInput placeholder='password' style={{borderWidth: 1, width: 300, height: 50, alignSelf: 'center', borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}/>
+     <Image source={require('../assets/login.png')} style={{width: 400, height: 420, objectFit: 'cover', alignSelf: 'center'}}/>
+      <View style={{justifyContent:'center', alignItems:'center', gap:20, backgroundColor:'#00ff99', paddingVertical: 50, borderTopLeftRadius: 30, borderTopRightRadius: 30}}>
+        <TextInput placeholder='Email' keyboardType='email-address' style={{borderWidth: 1, width: 300, height: 50, alignSelf: 'center', borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}/>
+        <TextInput placeholder='password' secureTextEntry={true} style={{borderWidth: 1, width: 300, height: 50, alignSelf: 'center', borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}/>
         <TouchableOpacity style={{borderWidth: 1, width: 300, height: 50, borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}>
           <Text style={{fontSize:20, fontWeight:'bold', color:'black', textAlign: 'center', alignSelf: 'center', paddingVertical: 10}}>Login</Text>
         </TouchableOpacity>
