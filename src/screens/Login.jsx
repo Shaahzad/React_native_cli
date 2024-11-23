cc07f27b473fdd9b01e715904cccd48d72f12c3d
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-23 14:19:52 +0500

    Done

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -6,7 +6,7 @@ const Login = () => {
     <ScrollView style={{flex:1}}>
      <Image source={require('../assets/login.png')} style={{width: 400, height: 420, objectFit: 'cover', alignSelf: 'center'}}/>
       <View style={{justifyContent:'center', alignItems:'center', gap:20, backgroundColor:'#00ff99', paddingVertical: 50, borderTopLeftRadius: 30, borderTopRightRadius: 30}}>
-        <TextInput placeholder='Email' keyboardType='email-address' style={{borderWidth: 1, width: 300, height: 50, alignSelf: 'center', borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}/>
+        <TextInput placeholder='Email@gmail.com' keyboardType='email-address' style={{borderWidth: 1, width: 300, height: 50, alignSelf: 'center', borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}/>
         <TextInput placeholder='password' secureTextEntry={true} style={{borderWidth: 1, width: 300, height: 50, alignSelf: 'center', borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}/>
         <TouchableOpacity style={{borderWidth: 1, width: 300, height: 50, borderRadius: 10, paddingHorizontal: 10, backgroundColor:'white'}}>
           <Text style={{fontSize:20, fontWeight:'bold', color:'black', textAlign: 'center', alignSelf: 'center', paddingVertical: 10}}>Login</Text>
