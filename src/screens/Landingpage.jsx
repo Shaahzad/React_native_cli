7911526d1e5237eb24e14221ee376bbfd3c55a6a
Muhammad shahzad <shehzadmemon147@gmail.com>
2024-11-24 14:41:22 +0500

    Done

--- src/screens/Landingpage.jsx
+++ src/screens/Landingpage.jsx
@@ -1,17 +1,37 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Dimensions, Image } from 'react-native'
 import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialIcons from "react-native-vector-icons/MaterialIcons"
+
+
+
+const SCREEN_HEIGHT = Dimensions.get("screen").height;
 
 const Landingpage = () => {
   return (
     <View style={styles.container}>
+      {/* Header   */}
         <View style={styles.header}>
-         <Text>ByKea</Text>
+         <Ionicons name="menu" size={24} color="black" />
+         <Text style={styles.headertxt}>BYKEA</Text>
+         <MaterialIcons name="add-call" size={24} color="black" />
         </View>
+
+     {/* banner image view */}
+      <View style={styles.bannerimgview}>
+       <Image source={require('../assets/banner.png')} style={styles.bannerImg}/>
+      </View>
+  {/* info view */}
+  <View style={styles.infoview}>
+  <MaterialIcons name="attach-money" size={24} color="green" />
+  <MaterialIcons name="message" size={24} color="green" />
+  </View>
+   
+
+   {/* Bottom Container */}
     </View>
   )
 }
-
-
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -24,6 +44,40 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
         paddingHorizontal: 10
+    },
+    headertxt: {
+      fontWeight: 'bold',
+      letterSpacing: 3,
+      fontSize: 21,
+      paddingHorizontal: 16      
+    },
+    bannerimgview:{
+      height: SCREEN_HEIGHT / 3.7,
+      backgroundColor: "#DEE3E8",
+      paddingHorizontal: 30,
+      paddingTop: 10
+    },
+    bannerImg:{
+      height: "84%",
+      borderRadius: 12
+    },
+    infoview:{
+      height: 40,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+      borderRadius: 12,
+      marginTop: -15,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.18,
+      shadowRadius: 1.00,
+      elevation: 1,
+      marginHorizontal: 20
     }
 })
 
